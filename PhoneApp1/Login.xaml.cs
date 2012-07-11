a17a7646c2ad11a5e22e4f6336a1745f1b89f7a3
Nathan Totten <ntotten@microsoft.com>
2012-07-11 11:37:26 -0500

    Initial code

---
+++ PhoneApp1/Login.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Facebook;
+using System.IO.IsolatedStorage;
+
+namespace PhoneApp1
+{
+    public partial class Login : PhoneApplicationPage
+    {
+        string oauthUrl = "https://www.facebook.com/dialog/oauth?client_id={0}&redirect_uri={1}&display=touch&response_type=token";
+        string facebookAppId = "183642511732233";
+        string redirectUri = "https://www.facebook.com/connect/login_success.html";
+
+        public Login()
+        {
+            InitializeComponent();
+            webBrowser1.Navigated += new EventHandler<System.Windows.Navigation.NavigationEventArgs>(webBrowser1_Navigated);
+            webBrowser1.Navigate(new Uri(String.Format(oauthUrl, facebookAppId, redirectUri)));
+        }
+
+        void webBrowser1_Navigated(object sender, System.Windows.Navigation.NavigationEventArgs e)
+        {
+            if (e.Uri.ToString().StartsWith(redirectUri))
+            {
+                var client = new FacebookClient();
+                var result = client.ParseOAuthCallbackUrl(e.Uri);
+                var accessToken = result.AccessToken;
+
+                var navUri = String.Format("/Welcome.xaml?access_token={0}", accessToken);
+                NavigationService.Navigate(new Uri(navUri, UriKind.Relative));
+            }
+        }
+    }
+}
---
+++ PhoneApp1/Welcome.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace PhoneApp1
+{
+    public partial class Welcome : PhoneApplicationPage
+    {
+        public Welcome()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            if (this.NavigationContext.QueryString.ContainsKey("access_token"))
+            {
+                textBlock2.Text = this.NavigationContext.QueryString["access_token"];
+            }
+            base.OnNavigatedTo(e);
+        }
+    }
+}
